0f68c64ff470bb81ae9c9d4f082a5090eb3677c0
Vijay <vijayendran91@gmail.com>
2016-06-15 16:20:28 +0000

    added shot.html.erb and added the flash comments for validating title and description

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,20 +2,31 @@ class ArticlesController < ApplicationController
   
   def new
     @article=Article.new
-   
-    
   end
 
   def create
-    render plain: params[:article].inspect
+    #render plain: params[:article].inspect
    
     @article=Article.new(article_params)
     @article.save
-    redirect_to articles_show(@artcle)
-  end
+    if @article.save
+      redirect_to article_path(@article)
+      flash[:notice]="article was successfully created"
+    else
+      render 'new'
+    end
+    
+  end 
   
-  def article_params
+ private def article_params
     params.require(:article).permit(:title, :description)
   end
   
+  def show
+    @article=Article.find(params[:id])
+  end
+
 end
+
+
+
